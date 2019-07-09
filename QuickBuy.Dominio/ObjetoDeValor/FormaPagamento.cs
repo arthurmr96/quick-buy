08ee6d95b847757366355892fcb3d9c0e7476ac0
Arthur Ribeiro <arthurmr13@hotmail.com>
2019-07-09 11:33:28 -0300

    Cria entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,20 @@
+﻿using QuickBuy.Dominio.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    class FormaPagamento
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool EhBoleto {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+    }
+}
