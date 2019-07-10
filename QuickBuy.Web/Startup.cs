318296d4310fdd1ea16e8ab415da4138505c0eb7
Arthur Ribeiro <arthurmr13@hotmail.com>
2019-07-10 17:39:09 -0300

    Começa a criar API para o produto

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuickBuy.Dominio.Contratos;
 using QuickBuy.Repositorio.Contexto;
+using QuickBuy.Repositorio.Repositorios;
 
 namespace QuickBuy.Web
 {
@@ -32,6 +34,7 @@ namespace QuickBuy.Web
                                                         option.UseLazyLoadingProxies()
                                                         .UseMySql(connectionString, 
                                                             mysql => mysql.MigrationsAssembly("QuickBuy.Repositorio")));
+            services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
