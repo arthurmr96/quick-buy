c0ca546128c4a78f44277fb4792a44c6470494da
Arthur Ribeiro <arthurmr13@hotmail.com>
2019-07-10 17:28:34 -0300

    Finaliza CRUD do BaseRepositorio e extende de forma correta nos outros repositorios dele

--- QuickBuy.Repositorio/Repositorios/ItemPedidoRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/ItemPedidoRepositorio.cs
@@ -1,16 +1,16 @@
 ﻿using QuickBuy.Dominio.Contratos;
 using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Contexto;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
-    class ItemPedidoRepositorio : BaseRepositorio<ItemPedido>, IItemPedidoRepositorio
+    public class ItemPedidoRepositorio : BaseRepositorio<ItemPedido>, IItemPedidoRepositorio
     {
-        public ItemPedidoRepositorio()
+        public ItemPedidoRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
         {
-
         }
     }
 }
